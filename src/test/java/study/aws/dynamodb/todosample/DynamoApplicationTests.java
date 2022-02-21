08a6f54520fd2efce4f0dea14b1c2a02509f1fab
ivanshevtsov <ivan.shevtsov@hazelcast.com>
2022-02-21 09:14:10 +0200

    initial project

---
+++ src/test/java/study/aws/dynamodb/todosample/DynamoApplicationTests.java
@@ -0,0 +1,13 @@
+package study.aws.dynamodb.todosample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
